ab4c636122bb925402bc2b4218d2d14875d6c9e7
Norrismi <Norrismi0@gmail.com>
2021-11-23 14:13:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState('');
+
+  let addTodo = (e) => {
+    e.preventDefault()
+
+      setTodos([...todos, input]);
+      setInput("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <form>
+
+      <input type="text" value={input} onChange={e => setInput(e.target.value)} />
+      <button type='submit' onClick={addTodo}>Add Todo</button>
+
+    </form>
+
+
+
+      <div>
+        {todos.map(todo => (
+          <p key={todo}>{todo}</p>
+        ))}
+      </div>
+
+
+
     </div>
   );
 }
